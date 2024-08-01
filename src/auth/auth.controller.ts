60a37f1c0b829d6fdfcaa6497d2fa69962304df4
joletuar <joletuar.dev@gmail.com>
2024-07-31 22:32:29 -0500

    feat: add dtos for perfom queries and rename of route names

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 import { NATS_SERVICE } from '../config';
+import { LogingUserDto } from './dtos/login.user.dto';
+import { RegisterUserDto } from './dtos/register.user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
-  @Post('register-user')
-  registerUser() {
-    return this.client.send('auth.register.user', {});
+  @Post('login-user')
+  logginUser(@Body() logingUserDto: LogingUserDto) {
+    return this.client.send('auth.login.user', logingUserDto);
   }
 
-  @Post('login-user')
-  logginUser() {
-    return this.client.send('auth.login.user', {});
+  @Post('register-user')
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto);
   }
 
   @Post('verify-token')
