a4e2e1cad0dec00e21d558bfb6e8b1fc95aab6cb
joletuar <joletuar.dev@gmail.com>
2024-08-26 09:57:04 -0500

    chore: delete guard from provider to check if the app continues working

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
-import { AuthGuard } from './guards/auth/auth.guard';
 import { RabbitMqModule } from 'src/transports/rabbitmq.module';
 
 @Module({
   imports: [RabbitMqModule],
   controllers: [AuthController],
-  exports: [AuthGuard],
-  providers: [AuthGuard],
 })
 export class AuthModule {}
