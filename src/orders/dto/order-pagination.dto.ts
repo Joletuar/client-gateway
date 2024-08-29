9a45e46f4c996eb96d4be46c0821facef22cb0a5
joletuar <joletuar.dev@gmail.com>
2024-08-28 22:43:44 -0500

    chore: order imports

--- src/orders/dto/order-pagination.dto.ts
+++ src/orders/dto/order-pagination.dto.ts
@@ -1,5 +1,6 @@
 import { IsEnum, IsOptional } from 'class-validator';
-import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+import { PaginationDto } from '../../common/dtos/pagination.dto';
 import { OrderStatus } from '../enums/order-status.enum';
 
 export class OrderPaginationDto extends PaginationDto {
