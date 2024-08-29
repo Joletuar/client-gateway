2fe774100574f88a06c92b98b7c4d8c2140c48ac
joletuar <joletuar.dev@gmail.com>
2024-08-28 22:43:35 -0500

    chore: delete comments

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -3,30 +3,10 @@ import { Type } from 'class-transformer';
 import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { OrdenItemDto } from './order-item.dto';
 
-// export class CreateOrderDto {
-//   @IsNumber()
-//   @IsPositive()
-//   totalAmount: number;
-
-//   @IsNumber()
-//   @IsPositive()
-//   totalItems: number;
-
-//   @IsEnum(OrderStatus, {
-//     message: `Possible status values are ${Object.values(OrderStatus)}`,
-//   })
-//   @IsOptional()
-//   status: OrderStatus = OrderStatus.PENDING;
-
-//   @IsBoolean()
-//   @IsOptional()
-//   paid: boolean = false;
-// }
-
 export class CreateOrderDto {
   @IsArray() // Validar que sea un array
   @ArrayMinSize(1) // Validar que tenga mínimo un elemento
   @ValidateNested({ each: true }) // Validamos cada elemento
-  @Type(() => OrdenItemDto)
+  @Type(() => OrdenItemDto) // transforma al tipo especificado
   items: Array<OrdenItemDto>;
 }
